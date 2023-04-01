7ad2484780f89b7435414d5a17f0e90f82067d8c
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-04-02 00:58:07 +0530

    File and FOlder structure and Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Navbar from "@/scenes/navbar";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from './theme'
+import { CssBaseline , ThemeProvider, Box } from '@mui/material'
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
-      App
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>predictions page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
