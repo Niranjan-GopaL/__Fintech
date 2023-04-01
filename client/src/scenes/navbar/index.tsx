0db326af9cf6fe60f085ae576af7b5b95705ecbb
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-04-02 01:14:22 +0530

    some more progress

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,51 +1,53 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-// import PixIcon from "@mui/icons-material";
+// import ApiIcon from '@mui/icons-material/Api';
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const { palette } = useTheme();
-  const [selected, setSelected] = useState("dashboard");
-  return (
-    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
-      {/* LEFT SIDE */}
-      <FlexBetween gap="0.75rem">
-        {/* <PixIcon sx={{ fontSize: "28px" }} /> */}
-        <Typography variant="h4" fontSize="16px">
-          Finanseer
-        </Typography>
-      </FlexBetween>
+    const { palette } = useTheme();
 
-      {/* RIGHT SIDE */}
-      <FlexBetween gap="2rem">
+    // to know which state we are on, for highlighting the text based on it   
+    const [selected, setSelected] = useState("dashboard");
+    return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+        {/* LEFT SIDE IN THE NAVBAR : ICON OF APP*/}
+        <FlexBetween gap="0.75rem">
+        {/* <ApiIcon sx={{ fontSize: "28px" }} /> */}
+        <Typography variant="h4" fontSize="16px">
+            Finanseer
+        </Typography>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="2rem">
         <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
+            <Link
             to="/"
             onClick={() => setSelected("dashboard")}
             style={{
-              color: selected === "dashboard" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
+                color: selected === "dashboard" ? "inherit" : palette.grey[700],
+                textDecoration: "inherit",
             }}
-          >
+            >
             dashboard
-          </Link>
+            </Link>
         </Box>
         <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
+            <Link
             to="/predictions"
             onClick={() => setSelected("predictions")}
             style={{
-              color: selected === "predictions" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
+                color: selected === "predictions" ? "inherit" : palette.grey[700],
+                textDecoration: "inherit",
             }}
-          >
+            >
             predictions
-          </Link>
+            </Link>
         </Box>
-      </FlexBetween>
+        </FlexBetween>
     </FlexBetween>
   );
 };
